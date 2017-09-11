8a8f635f4a057b1edf5f8447d57046f579ea8980
alexgonca <alexandre.reportagem@gmail.com>
2017-09-11 12:49:28 -0400

    implement match_case and match_entire_cell_content

--- replace.py
+++ replace.py
@@ -1,3 +1,5 @@
+import re
+
 class Importable:
     @staticmethod
     def __init__(self):
@@ -10,6 +12,18 @@ class Importable:
     @staticmethod
     def render(wf_module, table):
         to_replace = wf_module.get_param_string('to_replace')
+        match_case = wf_module.get_param_checkbox("match_case")
+        match_entire = wf_module.get_param_checkbox("match_entire")
         value = wf_module.get_param_string('value')
-        df = table.replace(to_replace, value)
+
+        to_replace = re.escape(to_replace)
+        if match_entire:
+            to_replace = '^' + to_replace + '$'
+
+        if match_case:
+            pattern = re.compile(to_replace)
+        else:
+            pattern = re.compile(to_replace, re.IGNORECASE)
+
+        df = table.replace(pattern, value)
         return df
