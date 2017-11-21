4d826a0f9e9cea92c7f9e89848bd41b8c0adaf02
Jonathan Stray <jonathanstray@gmail.com>
2017-11-21 12:14:24 -0800

    Update to new API

--- replace.py
+++ replace.py
@@ -1,29 +1,17 @@
-import re
+def render(table, params):
+    to_replace = params['to_replace']
+    match_case = params['match_case']
+    match_entire = params['match_entire']
+    value = params['value']
 
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
+    to_replace = re.escape(to_replace)
+    if match_entire:
+        to_replace = '^' + to_replace + '$'
 
-    @staticmethod
-    def event():
-        pass
+    if match_case:
+        pattern = re.compile(to_replace)
+    else:
+        pattern = re.compile(to_replace, re.IGNORECASE)
 
-    @staticmethod
-    def render(wf_module, table):
-        to_replace = wf_module.get_param_string('to_replace')
-        match_case = wf_module.get_param_checkbox("match_case")
-        match_entire = wf_module.get_param_checkbox("match_entire")
-        value = wf_module.get_param_string('value')
-
-        to_replace = re.escape(to_replace)
-        if match_entire:
-            to_replace = '^' + to_replace + '$'
-
-        if match_case:
-            pattern = re.compile(to_replace)
-        else:
-            pattern = re.compile(to_replace, re.IGNORECASE)
-
-        df = table.replace(pattern, value)
-        return df
+    df = table.replace(pattern, value)
+    return df
