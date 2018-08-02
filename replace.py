5659abf72d212adb1ae644c36373ed1e0c5baabb
emrig <emrig@users.noreply.github.com>
2018-08-01 23:35:54 -0400

    Handles categories

--- replace.py
+++ replace.py
@@ -1,6 +1,6 @@
 def render(table, params):
     # if no column has been selected, return table
-    if not params['colnames']:
+    if not params['colnames'] or not params['to_replace']:
         return table
 
     to_replace = params['to_replace']
@@ -10,6 +10,12 @@ def render(table, params):
     regex = params['regex']
     columns = params['colnames'].split(',')
     columns = [c.strip() for c in columns]
+
+    # Detect if input is a number or string
+    try:
+        replace_with = int(replace_with)
+    except ValueError:
+        pass
 
     if not regex:
         to_replace = re.escape(to_replace)
@@ -28,5 +34,33 @@ def render(table, params):
     else:
         pattern = re.compile(to_replace, re.IGNORECASE)
 
-    table[columns] = table[columns].astype(str).replace(pattern, replace_with)
+    for column in columns:
+        if pd.api.types.infer_dtype(table[column]) != 'categorical':
+            # Convert column to string (or retain category) if regex
+            dtype = table[column].dtype
+            table[column] = table[column].astype(str).replace(pattern, str(replace_with))
+            # try to convert back to dtype
+            table[column] = table[column].astype(dtype, errors='ignore')
+
+        # for categories, perform replace on index
+        else:
+            # All to string for now, .replace does not work on numerical categories
+            cat_dtype = table[column].cat.categories.dtype
+            new_index = pd.Series(table[column].cat.categories).astype(str).replace(pattern, str(replace_with))
+            # Check if duplicates
+            if not any(new_index.duplicated()):
+                # Try to cast back to original dtype
+                try:
+                    table[column] = table[column].cat.rename_categories(new_index.astype(cat_dtype))
+                except (TypeError, ValueError):
+                    table[column] = table[column].cat.rename_categories(new_index)
+            else:
+                category_mask = table[column].cat.categories.str.contains(pattern)
+                diff = set(new_index) - set(table[column].cat.categories)
+                table[column].cat.add_categories(diff, inplace=True)
+                series_index = table[column].cat.codes[lambda x: category_mask[x]].index
+
+                table[column].iloc[series_index] = table[column][series_index].str.replace(pattern, str(replace_with))
+                table[column].cat.remove_unused_categories(inplace=True)
+
     return table
--- test_replace.py
+++ test_replace.py
@@ -10,18 +10,31 @@ class TestReplace(unittest.TestCase):
         # Test data includes:
         #  - rows of numeric and string types
         self.table = pd.DataFrame([
-            ['AaAfredaAaA', 12333, 3.14, '1964-5-05'],
-            ['aAaAfredaAAaaa', 5211111, 45.64, '1964-7-28 08:55'],
-            ['AaAfredAaaaA', -323434, 435.00, '1964'],
-            ['AAfredAA', 32331, 35331.19, '1964-2-27'],
-            ['AaaAfredAaaaA', 8009823, 0.07, '1964-7-30']],
-            columns=['stringcol', 'intcol', 'floatcol', 'datecol'])
+            ['AaAfredaAaA', 12333, 3.14, '1964-5-05', 'a'],
+            ['aAaAfredaAAaaa', 5211111, 45.64, '1964-7-28 08:55', 'b'],
+            ['AaAfredAaaaA', -323434, 435.00, '1964', 'c'],
+            ['AAfredAA', 32331, 35331.19, '1964-2-27', 'd'],
+            ['AaaAfredAaaaA', 8009823, 0.07, '1964-7-30', 'c']],
+            columns=['stringcol', 'intcol', 'floatcol', 'datecol', 'catcol'])
 
         # Pandas should infer these types anyway, but leave nothing to chance
         self.table['stringcol'] = self.table['stringcol'].astype(str)
         self.table['intcol'] = self.table['intcol'].astype(np.int64)
         self.table['floatcol'] = self.table['floatcol'].astype(np.float64)
         self.table['datecol'] = self.table['datecol'].astype(str)
+        self.table['catcol'] = self.table['catcol'].astype('category')
+
+        self.simple_table = pd.DataFrame([
+            [201, 201, '201'],
+            [201, 201, '201'],
+            [100, 100, '100']]
+            , columns=['intcol', 'catcol', 'catcol2'])
+
+        self.simple_table['intcol'] = self.simple_table['intcol'].astype(np.int64)
+        self.simple_table['catcol'] = self.simple_table['catcol'].astype('category')
+        self.simple_table['catcol2'] = self.simple_table['catcol2'].astype('category')
+
+        self.temp_table = pd.read_csv('bin/general_10000.csv', encoding='ISO-8859-1', dtype='category')
 
     def test_NOP(self):
         params = {'colnames': ''}
@@ -69,7 +82,7 @@ class TestReplace(unittest.TestCase):
                 self.assertTrue(y == '1964')
 
     def test_regex_num(self):
-        # intcol should only contain '2'
+        # intcol should only contain '2' and convert to string
         params = {
             'colnames': 'intcol',
             'to_replace': '[0-1|3-9|\D]',
@@ -79,11 +92,11 @@ class TestReplace(unittest.TestCase):
             'replace_with': ''
         }
         out = render(self.table.copy(), params)
-        self.assertTrue(out['intcol'].dtype == object)
+        self.assertTrue(out['intcol'].dtype == np.int64)
         for y in out['intcol']:
-            self.assertTrue(y == '2')
+            self.assertTrue(y == 2)
 
-        # floacol should only contain '2.'
+        # floatcol should only contain '2.'
         params = {
             'colnames': 'floatcol',
             'to_replace': '[\d]',
@@ -122,9 +135,82 @@ class TestReplace(unittest.TestCase):
             'replace_with': ''
         }
         out = render(self.table.copy(), params)
-        self.assertTrue(out['intcol'].dtype == object)
+        self.assertTrue(out['intcol'].dtype == np.int64)
         for y in out['intcol']:
             self.assertTrue(y != '2')
+
+    def test_cat_search(self):
+        # catcol remain category with updated index
+        params = {
+            'colnames': 'catcol',
+            'to_replace': 'c',
+            'match_case': False,
+            'regex': False,
+            'match_entire': False,
+            'replace_with': 'e'
+        }
+        out = render(self.table.copy(), params)
+        self.assertTrue(out['catcol'].dtype == 'category')
+        self.assertTrue(set(out['catcol'].cat.categories) == set(['a', 'b', 'd', 'e']))
+        self.assertEqual(out['catcol'].tolist(), ['a','b','e','d','e'])
+
+        # catcol remain category with updated index
+        params = {
+            'colnames': 'catcol',
+            'to_replace': '[a|c]',
+            'match_case': False,
+            'regex': True,
+            'match_entire': False,
+            'replace_with': '!'
+        }
+        out = render(self.table.copy(), params)
+        self.assertTrue(out['catcol'].dtype == 'category')
+        self.assertTrue(set(out['catcol'].cat.categories) == set(['b', 'd', '!']))
+        self.assertEqual(out['catcol'].tolist(), ['!', 'b', '!', 'd', '!'])
+
+        # catcol remain category with updated index
+        params = {
+            'colnames': 'catcol',
+            'to_replace': 'a',
+            'match_case': False,
+            'regex': False,
+            'match_entire': False,
+            'replace_with': '99'
+        }
+        out = render(self.table.copy(), params)
+        self.assertTrue(out['catcol'].dtype == 'category')
+
+        # catcol drop multiple rows
+        params = {
+            'colnames': 'catcol',
+            'to_replace': '[a|c]',
+            'match_case': False,
+            'regex': True,
+            'match_entire': False,
+            'replace_with': ''
+        }
+        out = render(self.table.copy(), params)
+        self.assertTrue(out['catcol'].dtype == 'category')
+        self.assertTrue(set(out['catcol'].cat.categories) == set(['b', 'd', '']))
+        self.assertEqual(out['catcol'].tolist(), ['', 'b', '', 'd', ''])
+
+        # catcol drop multiple rows
+        params = {
+            'colnames': 'datecol',
+            'to_replace': '[^\w\s]',
+            'match_case': False,
+            'regex': True,
+            'match_entire': False,
+            'replace_with': ''
+        }
+        modified_table = self.table.copy()
+        modified_table['datecol'] = modified_table['datecol'].astype('category')
+
+        out = render(modified_table, params)
+        self.assertTrue(out['datecol'].dtype == 'category')
+        expected_cats = set(['1964505', '1964728 0855', '1964', '1964227', '1964730'])
+        self.assertTrue(set(out['datecol'].cat.categories) == expected_cats)
+        self.assertEqual(set(out['datecol'].tolist()), expected_cats)
 
     def test_exact(self):
         # datecol[2] should be none
@@ -158,6 +244,51 @@ class TestReplace(unittest.TestCase):
                 self.assertTrue(not value)
             else:
                 self.assertTrue(value)
+
+
+    def test_multiple_coltypes(self):
+        # intcol converts to string, catcol converts index to string
+        params = {
+            'colnames': 'intcol,catcol,catcol2',
+            'to_replace': '1',
+            'match_case': False,
+            'regex': False,
+            'match_entire': False,
+            'replace_with': '!'
+        }
+        out = render(self.simple_table.copy(), params)
+        expected = pd.DataFrame([
+            ["20!", "20!", "20!"],
+            ["20!", "20!", "20!"],
+            ["!00", "!00", "!00"]]
+            , columns=['intcol', 'catcol', 'catcol2'])
+
+        expected['catcol'] = expected['catcol'].astype('category')
+        expected['catcol2'] = expected['catcol2'].astype('category')
+
+        self.assertTrue(out.equals(expected))
+
+        # intcol remains int, catcol remains int index
+        params = {
+            'colnames': 'intcol,catcol,catcol2',
+            'to_replace': '1',
+            'match_case': False,
+            'regex': False,
+            'match_entire': False,
+            'replace_with': '9'
+        }
+        out = render(self.simple_table.copy(), params)
+        expected = pd.DataFrame([
+            [209, 209, "209"],
+            [209, 209, "209"],
+            [900, 900, "900"]]
+            , columns=['intcol', 'catcol', 'catcol2'])
+
+        expected['intcol'] = expected['intcol'].astype(np.int64)
+        expected['catcol'] = expected['catcol'].astype('category')
+        expected['catcol2'] = expected['catcol2'].astype('category')
+
+        self.assertTrue(out.equals(expected))
 
 if __name__ == '__main__':
     unittest.main()
